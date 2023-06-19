ae801c18231faa51c55140de5645e6f9c6781715
정채운 <wun@jeongchaeun-ui-MacBookAir.local>
2023-06-19 16:26:57 +0900

    2nd Commit

--- src/main/java/com/example/demo/repository/TestRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.TestEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
-
-public interface TestRepository extends JpaRepository<TestEntity, String> {
-}
--- src/main/java/com/example/demo/service/TestService.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.service;
-
-import com.example.demo.controller.dto.JoinRequest;
-
-public interface TestService {
-    String join(JoinRequest joinRequest);
-//    String searchAll(JoinRequest joinRequest);
-}
